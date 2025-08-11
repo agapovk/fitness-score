c8c9ea770f751a9c34efb31b0f04acb55b412cf3
Agapov Konstantin <agapovk@gmail.com>
2025-08-11 22:23:06 +0500

    update4

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,13 @@
-import { BackButton } from "./BackButton";
+// import { BackButton } from "./BackButton";
+import { useLaunchParams } from "@telegram-apps/sdk-react";
 
 export default function Header() {
+	const launchParams = useLaunchParams();
 	return (
 		<header className="p-2 flex gap-2 bg-white text-black justify-between">
-			<BackButton />
-			<h1>Fit-score</h1>
+			{/* <BackButton /> */}
+			{/* <h1>Fit-score</h1> */}
+			{JSON.stringify(launchParams, null, 2)}
 		</header>
 	);
 }
